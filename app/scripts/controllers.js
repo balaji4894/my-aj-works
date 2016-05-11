b7b732cca1cde6a9fa523800868386772b741a46
Balaji Rajagopalan <brajagopalan@inautix.co.in>
2016-05-11 17:38:28 +0530

    End of week4 lesson2 with menu not ready

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,11 +8,17 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= [];
-            menuFactory.getDishes()
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+                        $scope.dishes= {};
+                        menuFactory.getDishes()
             .then(
                 function(response) {
                     $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
 
@@ -77,15 +83,20 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
             $scope.dish = {};
-            menuFactory.getDish(parseInt($stateParams.id,10))
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+                        menuFactory.getDish(parseInt($stateParams.id,10))
             .then(
                 function(response){
                     $scope.dish = response.data;
                     $scope.showDish=true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
             
-            $scope.dish = dish;
+            
             
         }])
 
@@ -109,13 +120,19 @@ angular.module('confusionApp')
         // implement the IndexController and About Controller here
         .controller('IndexController',['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
             $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+
             menuFactory.getDish(0)
             .then(
-                  function(response){
-                        $scope.dish = response.data;
-                        $scope.showDish = true;
-                  }
-             );
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
               $scope.recommendedDish=$scope.dish;
               console.log($scope.recommendedDish);
               console.log($scope.dish);
